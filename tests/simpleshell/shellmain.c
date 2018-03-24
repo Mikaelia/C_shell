c0b62ac1db2b87741ebbc362ecd25f6948678b67
Victor Nguyen <victormdnguyen88@gmail.com>
2018-03-24 21:48:25 +0000

    cp main file

---
+++ tests/simpleshell/shellmain.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+  * main - main function for simple shell
+  *
+  * Return: 0 on exit or fail
+  */
+int main(__attribute__((unused))int argc, char **argv)
+{
+	looper(argv);
+	return (0);
+}
