0b9d8f1daadb28c4cccba3d8f39017786b2c5e3c
Mikaelia <mikaelagurney@gmail.com>
2018-03-24 22:09:55 +0000

    Re-add launch

---
+++ tests/simpleshell/launch.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+/**
+  * launch - forks process and executes commands
+  * @argv: arg passed from stdin
+  *
+  * Return: 0
+ */
+int launch(char **av, char **tokens, char *line)
+{
+	pid_t child_pid;
+	char *executable;
+	static int count;
+
+	executable = NULL;
+	count = 0;
+
+	if (checkbuiltins(tokens) == -1) /*returns -1 on fail*/
+	{
+
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			printf("Fork error");
+			exit(1);
+		}
+		if (child_pid == 0)
+		{
+
+			{
+				if (execve(tokens[0], tokens, NULL) == -1)
+				{
+					count++;
+					executable = checkpath(tokens[0]); /*return NULL on fail*/
+					if (execve(executable, tokens, NULL) == -1)
+					{
+						printf("%i", count);
+						printerror(av, count, line);
+						exit(0);
+					}
+				}
+			}
+
+		}
+		else
+		{
+			child_pid = wait(NULL);
+		}
+	}
+	printf("%i", count);
+	return (1);
+}
---
+++ tests/simpleshell/looper.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+  * looper - continuous shell loop
+  *
+  *
+  *
+  *
+  */
+void looper(char **av)
+{
+	char *input;
+	char **tokens;
+	int status;
+
+	status = 0;
+	printf("This is arv[0]%s\n", av[0]);
+	do {
+		input = _prompt();		/*gets input stream*/
+		if (input == NULL)
+			status = -1;
+		tokens = tokenize(input);	/*splits input into tokens*/
+		if (!tokens)
+			status = -1;
+
+		status = launch(av, tokens, input);	/*executes tokens*/
+		if (status != 1)
+			status = -1;
+
+		free(input);
+		free(tokens);
+	} while (status);
+}
