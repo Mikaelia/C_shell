1362b90fdff528e84ed0df3a1bec1e2fdbfd947c
Mikaela Gurney <mikaelagurney@gmail.com>
2018-08-04 08:38:51 -0700

    Update findpathvar.c

--- findpathvar.c
+++ findpathvar.c
@@ -1,40 +1,41 @@
 #include "holberton.h"
 /**
- * path_match - searches env variable strings for PATH name
+ * path_match - searches env variable for PATH, and returns subsequent file path
  * @envar: environmental variable to compare
  *
  * Return: 1 on match, -1 if no match found
  */
 char *path_match(char *envar)
 {
-	char *path = "PATH=";
+	char *to_match = "PATH=";
 
 	if (envar == NULL)
 	{
 		perror("NULL ARGUMENT");
 		return (NULL);
 	}
-	while (*envar != '\0' && *path != '\0' && *envar == *path)
+	while (*envar != '\0' && *to_match != '\0' && *envar == *to_match)
 	{
 		envar++;
-		path++;
+		to_match++;
 	}
-	if (*path != '\0')
+	if (*to_match != '\0')
 	{
 		return (NULL);
 	}
+	// return string following 'PATH='
 	return (envar);
 }
 
 /**
   * findpath - finds the PATH variable within environmental variable list
   *
-  * Return: Pointer to path variable, or NULL on fail
+  * Return: Pointer to executable path, or NULL on fail
   */
 char *findpath()
 {
 	int i;
-	char *pathvar;
+	char *path;
 	char *findmatch;
 
 	i = 0;
@@ -42,15 +43,16 @@ char *findpath()
 
 	while (environ[i] != NULL)
 	{
+		// cycles through env variables
 		findmatch = path_match(environ[i]);
 		if (findmatch)
 		{
-			pathvar = findmatch;
+			path = findmatch;
 			break;
 		}
 		else
 			i++;
 	}
-	return (pathvar);
+	return (path);
 }
 
